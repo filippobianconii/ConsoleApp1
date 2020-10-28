0caba8cc5559bfa09696f3b460a19ae9c7ecf3c1
filippo.bianconi <filippo.bianconi.stud@ispascalcomandini.it>
2020-10-28 10:52:26 +0100

    Primo passo: aggiunta metodo ordinamento e individuazione del minimo

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vett = new int[10] { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+
+            Ordina(vett);
+            int mi = Minimo(vett);
+            Console.WriteLine("il valore m inimo è;" + mi);
+
+        }
+
+        static void Ordina(int [] vett)
+        {
+            Array.Sort(vett);
+        }
+
+        static int Minimo(int[] vett)
+        {
+            return vett[0];
+        }
+    }
+}
